12365a7f14608eabf80e8255c2201f635cbc16f2
Evgenii Dementev <63846200+ClickDaHouseCat@users.noreply.github.com>
2023-10-13 22:01:38 +0300

    feat: everyone run (Fri, Oct 13, 2023. 22:01:01 PM)

--- script.js
+++ script.js
@@ -1,24 +1,49 @@
-const start = document.getElementById('startRaceBtn')
-start.addEventListener('click',startGame )
 
-function startGame() {
-    var road1 = document.queryCommandIndeterm("road1");
-    var characters = document.querySelectorAll(".character");
+document.addEventListener('DOMContentLoaded', function () {
+    const blocks = document.querySelectorAll('.ch');
+    const container = document.querySelector('.image-container');
+    // Добавлены элементы для отображения информации о победителе
+    const winnerContainer = document.getElementById('winner-container');
+    const winnerMessage = document.getElementById('winner-message');
+    const winnerName = document.getElementById('winner-name');
+    const winnerImage = document.getElementById('winner-image');
 
-    var speed = 5; // Установите вашу начальную скорость
-    var interval = 20; // Интервал в миллисекундах
+    let speeds = Array.from({ length: blocks.length }, () => Math.random() * 100 + 50);
 
-    var moveCharacters = function () {
-      characters.style.transform = "translateX(" + speed + "px)";
+    function moveBlocksRight() {
+      let winnerBlock = null; // Блок, который первым достиг правого края
+      let winnerTime = Infinity; // Время, когда блок достиг правого края первым
 
-      // Проверка на достижение края дороги
-      if (characters.offsetLeft >= road1.offsetWidth) {
-        // Если персонаж достиг края родительского блока, завершаем игру
-        alert("Игра завершена");
-        clearInterval(gameInterval);
+      blocks.forEach(function (block, index) {
+        let currentLeft = parseInt(window.getComputedStyle(block).left);
+        let speed = speeds[index];
+
+        // Изменяем скорость в процессе на случайное значение от -10 до 10
+        speed += Math.random() * 20 - 10;
+
+        // Проверяем, достиг ли блок правого края родителя
+        if (currentLeft + speed + block.clientWidth >= container.clientWidth && currentLeft < container.clientWidth) {
+          if (currentLeft < winnerTime) {
+            winnerTime = currentLeft;
+            winnerBlock = block;
+          }
+        }
+
+        block.style.left = currentLeft + speed + 'px';
+        speeds[index] = speed;
+      });
+
+      if (winnerBlock !== null) {
+        // Отображаем блок с информацией о победителе
+        winnerMessage.innerHTML = `Победил ${winnerBlock.style.background}`;
+        winnerName.textContent = `Игрок ${winnerBlock.dataset.player}`;
+        winnerImage.src = `./assets/gif/${winnerBlock.dataset.player}.gif`;
+        winnerContainer.style.display = 'block';
+        return; // Прекращаем движение
       }
-    };
 
-    var gameInterval = setInterval(moveCharacters, interval);
-  }
+      setTimeout(moveBlocksRight, 100);
+    }
 
+    moveBlocksRight();
+  });
